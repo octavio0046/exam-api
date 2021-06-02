e09bf0d44eb9b4e7742913cfbcf8dc5f048abe02
Octavio Herrera <octavioherreraing@gmail.com>
2021-06-01 20:36:56 -0600

    OCTAVIO: crud vehiculos

---
+++ src/interface/vehiculos.ts
@@ -0,0 +1,12 @@
+export interface IVehiculos {
+    id: number;
+    Modelo:string;
+    TCMarcaId:number;
+    Precio:number;
+    Color:string;
+    Tipo:string;
+    Traccion:string;
+    Estado:string;
+    TCConcesionarioId:number;
+    Created_At:Date;
+}
