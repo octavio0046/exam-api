9866a551e20ae5757a4458835208dc32829149c3
Octavio Herrera <octavioherreraing@gmail.com>
2021-06-04 16:27:57 -0600

    OCTAVIO: arreglo de test cotizaciones

--- test/test_methods/8cotizaciones.methods.test.ts
+++ test/test_methods/8cotizaciones.methods.test.ts
@@ -1,11 +1,22 @@
 import {  connectTest } from "../../database";
 import { ICotizaciones } from "../../src/interface/cotizaciones";
 
+
 export async function getAll() {
+  const newDato: any = {
+    FechaInicio: "",
+    FechaFin:"",
+  };
+
   const conn = await connectTest();
-  await conn.query("SELECT * FROM TCCotizaciones");
+  await conn.query('select  co.*,ag.Nombres as Agente, cli.Nombres as Cliente from tccotizaciones co inner join tcagentes ag on ag.id=co.tcAgenteid inner join tcclientes cli on cli.id = co.tcclienteid where date(co.created_at) between date(ifnull(?,co.created_at))and date(ifnull(?,co.created_at) );', [newDato.FechaInicio,newDato.FechaFin]);
   return true;
 }
+// export async function getAll() {
+//   const conn = await connectTest();
+//   await conn.query('select  co.*,ag.Nombres as Agente, cli.Nombres as Cliente from tccotizaciones co inner join tcagentes ag on ag.id=co.tcAgenteid inner join tcclientes cli on cli.id = co.tcclienteid where date(co.created_at) between date(ifnull(?,co.created_at))and date(ifnull(?,co.created_at) );', [req.body.FechaInicio,req.body.FechaFin]);
+//   return true;
+// }
 
 export async function create() {
   const newDato: ICotizaciones = {
